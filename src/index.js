673cc385991eef65625d28ccd2c5b72eace4e189
DoubleR0 <rewkung43@gmail.com>
2020-11-09 13:58:14 +0700

    change pkj

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import resolvers from './resolvers.js';
 import express from 'express';
 import bodyParser from 'body-parser';
 import pkg from 'apollo-server-express';
-import pkg from 'graphql-tools';
-const { makeExecutableSchema } = pkg;
 const { graphqlExpress, graphiqlExpress } = pkg;
+import pkj from 'graphql-tools';
+const { makeExecutableSchema } = pkj;
 
 const uri = "mongodb+srv://admin:admin@cluster0.ze1ps.gcp.mongodb.net/FarmhugDB?retryWrites=true&w=majority";
 
